9ff5307f2867d87aa8ca8a574a4653ef8b705317
Pavlo Rosa <pavlo.rosa@gmail.com>
2020-06-09 18:24:19 +0200

    + Recipe Domain basic fields

--- recipes/models.py
+++ recipes/models.py
@@ -4,6 +4,13 @@ from djongo import models
 from django import forms
 
 
+class ListText(models.Model):
+  text = models.CharField(max_length=255)
+
+  class Meta:
+    abstract = True
+
+
 class LangText(models.Model):
   name = models.CharField(max_length=255)
   lang = models.CharField(max_length=3)
@@ -23,7 +30,7 @@ class Category(models.Model):
 
 class IngredientItem(models.Model):
   ingredient = models.CharField(max_length=64)
-  measure = models.CharField(max_length=64)
+  measure = models.CharField(max_length=64, null=True, blank=True)
 
   class Meta:
     abstract = True
@@ -32,10 +39,13 @@ class IngredientItem(models.Model):
 class IngredientItemForm(forms.ModelForm):
   class Meta:
     model = IngredientItem
-    fields = ('ingredient',)
+    fields = ('ingredient','measure')
 
 
 class Recipe(models.Model):
+  name = models.CharField(max_length=256)
+  lang = models.CharField(max_length=256)
+
   ingredients = models.ArrayField(
     model_container=IngredientItem,
     model_form_class=IngredientItemForm
@@ -44,7 +54,12 @@ class Recipe(models.Model):
     model_container=Category
   )
   description = models.TextField()
-  image = models.CharField(max_length=256)
+  image = models.TextField()
   video = models.CharField(max_length=256)
   country = models.CharField(max_length=256)
+  tags = models.ArrayField(
+    model_container=ListText
+  )
 
+  def __str__(self):
+    return str(self.id)+": "+ self.name
